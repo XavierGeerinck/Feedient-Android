bb8ea171b950494913f7d7d9cc61af0818cf1567
Mo Binni <mohamad.el-binni@student.kdg.be>
2014-03-25 00:28:30 +0100

    test project for sliding menu tabs

---
+++ Test projects/Slidingtabs/app/src/main/java/com/slidingtabs/app/fragments/PoliticiFragment.java
@@ -0,0 +1,65 @@
+package com.slidingtabs.app.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.slidingtabs.app.PagerSlidingTabStrip;
+import com.slidingtabs.app.R;
+import com.slidingtabs.app.SuperAwesomeCardFragment;
+
+/**
+ * Created by M on 25/03/2014.
+ */
+public class PoliticiFragment extends Fragment {
+
+    public PoliticiFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        // Initialize the ViewPager and set an adapter
+        ViewPager pager = (ViewPager) rootView.findViewById(R.id.pager);
+        pager.setAdapter(new MyPagerAdapter(getActivity().getSupportFragmentManager()));
+
+        // Bind the tabs to the ViewPager
+        PagerSlidingTabStrip tabs = (PagerSlidingTabStrip) rootView.findViewById(R.id.tabs);
+        tabs.setViewPager(pager);
+        return rootView;
+    }
+
+    public class MyPagerAdapter extends FragmentPagerAdapter {
+
+        private final String[] TITLES = { "Categories", "Home", "Top Paid", "Top Free", "Top Grossing", "Top New Paid",
+                "Top New Free", "Trending" };
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return TITLES[position];
+        }
+
+        @Override
+        public int getCount() {
+            return TITLES.length;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return SuperAwesomeCardFragment.newInstance(position);
+        }
+
+    }
+}
