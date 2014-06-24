216dd1c48dd6bdf126080d8a20eb6eed7c9e470f
Xavier Geerinck <thebillkidy@hotmail.com>
2014-06-24 15:29:36 +0200

    Added pull to update

--- app/src/main/java/com/feedient/android/activities/ViewAllFeedsActivity.java
+++ app/src/main/java/com/feedient/android/activities/ViewAllFeedsActivity.java
@@ -1,19 +1,27 @@
 package com.feedient.android.activities;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import com.feedient.android.R;
 import com.feedient.android.adapters.ItemArrayAdapter;
 import com.feedient.android.models.ViewAllFeeds;
 
+import uk.co.senab.actionbarpulltorefresh.library.ActionBarPullToRefresh;
 import uk.co.senab.actionbarpulltorefresh.library.PullToRefreshLayout;
+import uk.co.senab.actionbarpulltorefresh.library.listeners.OnRefreshListener;
 
 import java.util.Observable;
 import java.util.Observer;
 
-public class ViewAllFeedsActivity extends ListActivity implements Observer {
+public class ViewAllFeedsActivity extends Activity implements Observer, OnRefreshListener {
     private ItemArrayAdapter itemArrayAdapter;
     private ViewAllFeeds viewAllFeeds;
     private PullToRefreshLayout mPullToRefreshLayout;
+    private ListView mListView;
 
     /**
      * Called when the activity is first created.
@@ -21,14 +29,21 @@ public class ViewAllFeedsActivity extends ListActivity implements Observer {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.view_all_feeds); // @todo: When loading, set a loading icon
+        setContentView(R.layout.view_all_feeds); // @todo: When loading, set a loading icon
 
         viewAllFeeds = new ViewAllFeeds(this);
         viewAllFeeds.addObserver(this);
         viewAllFeeds.loadFeeds();
 
+        mListView = (ListView)findViewById(R.id.list);
         itemArrayAdapter = new ItemArrayAdapter(this, viewAllFeeds.getFeedPosts());
-        setListAdapter(itemArrayAdapter);
+        mListView.setAdapter(itemArrayAdapter);
+
+        mPullToRefreshLayout = (PullToRefreshLayout)findViewById(R.id.swipe_container);
+        ActionBarPullToRefresh.from(this)
+                .allChildrenArePullable()
+                .listener(this)
+                .setup(mPullToRefreshLayout);
     }
 
     @Override
@@ -48,5 +63,10 @@ public class ViewAllFeedsActivity extends ListActivity implements Observer {
 
     public void setItemArrayAdapter(ItemArrayAdapter itemArrayAdapter) {
         this.itemArrayAdapter = itemArrayAdapter;
+    }
+
+    @Override
+    public void onRefreshStarted(View view) {
+        viewAllFeeds.loadFeeds();
     }
 }
